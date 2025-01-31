98508277dcb809172c31e73b32cb59ab1d1fa513
Milos Ostojic <ostojicmilos10@gmail.com>
2025-01-30 20:51:01 -0500

    [10] Implement endpoint for uploading files to receive AI generated notes, and move AI routes to /app

---
+++ app/api/assistant/summary/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import { getAIService } from "@/services/ai/AIServiceFactory";
+
+export async function POST(req: Request) {
+    if (!req.headers.get("Content-Type")?.includes("multipart/form-data")) {
+        return NextResponse.json({ message: "Content-Type must be multipart/form-data" }, { status: 400 });
+    }
+
+    try {
+        // Get form data and check file is provided
+        const formData = await req.formData();
+        const file = formData.get("file");
+        if (!file || !(file instanceof Blob)) {
+            return NextResponse.json({ message: "A file must be provided in the \'file\' field" }, { status: 400 })
+        }
+
+        // Check file type is an appropriate option
+        const allowedTypes = new Set(["text/plain", "application/pdf", "image/png", "image/jpeg"]);
+        if (!allowedTypes.has(file.type)) {
+            return NextResponse.json(
+                { message: `File must be one of the following types: ${Array.from(allowedTypes).join(", ")}` },
+                { status: 400 }
+            );
+        }
+
+        // Verify file size does not exceed 2MB (arbitrary limit, feel free to change)
+        if (file.size > 2e6) {
+            return NextResponse.json({ message: `File exceeds maximum file size` }, { status: 400 });
+        }
+
+        const aiService = getAIService();
+        const result = await aiService.generateNotes(file);
+        return NextResponse.json({ message: result });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: "Sorry, something went wrong." }, { status: 500 });
+    }
+    
+}
--- services/ai/GeminiService.ts
+++ services/ai/GeminiService.ts
@@ -9,9 +9,21 @@ export class GeminiService implements IAIService {
         this.model = genAI.getGenerativeModel({ model });
     }
 
-    async ask(question: string): Promise<{ message: string; }> {
+    async ask(question: string): Promise<string> {
         const prompt = `Format the response as human-readable HTML-safe content, without unnecessary characters like stars or double quotes. My question is: ${question}`
         const result = await this.model.generateContent(prompt);
-        return { message: result.response.text() };
+        return result.response.text();
+    }
+
+    async generateNotes(file: Blob): Promise<string> {
+        // Convert file to Base64 string
+        const arrayBuffer = await file.arrayBuffer();
+        const data = (Buffer.from(arrayBuffer)).toString("base64");
+
+        // Send request to gemini
+        const prompt = "Could you generate some brief notes from the given file? Format the response in a human-readable HTML-safe content, without unnecessary characters like stars or double quotes.";
+        const imagePart = { inlineData: { data: data, mimeType: file.type } };
+        const result = await this.model.generateContent([prompt, imagePart]);
+        return result.response.text();
     }
 }
