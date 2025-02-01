48e82b1f7392f6c172363e70ebd4f08b91054a5d
Ryan Ly <lyryan3@LAPTOP-EAF2RQ8H.localdomain>
2025-02-01 03:24:56 -0500

    Ticket 16 - Added backend API endpoints for creating/validating accounts, as well as two basic pages for testing these features.

---
+++ pages/login.js
@@ -0,0 +1,13 @@
+// pages/login.js
+import LoginForm from '../components/LoginForm';
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <h1>Notare Sign-In Page</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ pages/signup.js
@@ -0,0 +1,13 @@
+// pages/login.js
+import LoginForm from '../components/SignUpForm';
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <h1>Notare Sign-Up Page</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
