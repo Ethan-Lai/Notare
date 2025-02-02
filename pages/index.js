871ec3eca21dad710689f3dfe9ac4a288d00ef92
Ryan Ly <lyryan3@LAPTOP-EAF2RQ8H.localdomain>
2025-02-02 03:04:37 -0500

    [9] Fixed bug where saving stopped working on file upload

--- pages/index.js
+++ pages/index.js
@@ -14,36 +14,36 @@ export default function Home() {
 
   // function for handling uploads of notes
   const handleUploadNote = async (fileContent, title, tag, file) => {
-    const newNote = { title, content: fileContent, tag };
-    setNote(newNote); // update the contents of the note
+  const newNote = { title, content: fileContent, tag };
+  setNote(newNote); // update the contents of the note (for the text)
 
-    try {
-      // create form data for the API
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("title", title);
-      formData.append("tag", tag);
+  try {
+    // create form data for the API
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("title", title);
+    formData.append("tag", tag);
 
-      // send form data to endpoint
-      const response = await fetch('/api/assistant/summary', {
-        method: 'POST',
-        body: formData,
-      });
+    // send form data to ai endpoint
+    const response = await fetch('/api/assistant/summary', {
+      method: 'POST',
+      body: formData,
+    });
 
-      if (!response.ok) {
-        throw new Error('Failed to generate response.');
-      }
-
-      // change the chat box
-      const data = await response.json();
-      setPrevQuestion([...prev_question, title]);
-      setPrevResponse([...prev_response, data.message]);
-
-    } catch (error) {
-      console.error(error);
-      alert('Error uploading file: ' + error.message);
+    if (!response.ok) {
+      throw new Error('Failed to generate response.');
     }
-  };
+
+    // change the chat box
+    const data = await response.json();
+    setPrevQuestion([...prev_question, title]);
+    setPrevResponse([...prev_response, data.message]);
+
+  } catch (error) {
+    console.error(error);
+    alert('Error uploading file: ' + error.message);
+  }
+};
 
   return (
     <div style={{ display: 'flex', gap: '2rem', padding: '2rem' }}>
