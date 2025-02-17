9dba4077eaa6e4a4b299483a0edecda35bd47f27
Milos Ostojic <ostojicmilos10@gmail.com>
2025-02-17 11:57:19 -0500

    Add Mantine dependencies and implement providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
+import "@mantine/core/styles.css";
 import type { AppProps } from "next/app";
+import {createTheme, MantineProvider} from "@mantine/core";
+import Head from "next/head";
+
+const theme = createTheme({
+    // TODO: Set up theme here
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <MantineProvider theme={theme}>
+            <Head>
+                <title>Notare</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+                />
+                <link rel="shortcut icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </MantineProvider>
+    );
 }
