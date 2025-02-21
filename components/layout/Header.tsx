49da5bf31db78b21bf9d743af688a1c7000c56dd
mumark <mark.mu@mail.utoronto.ca>
2025-02-21 16:46:20 -0500

    Header.tsx remodified

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,8 +1,15 @@
-import {ActionIcon, AppShellHeader, Flex, Title, useMantineColorScheme} from "@mantine/core";
-import {IconMoon, IconSun} from "@tabler/icons-react";
 import { useEffect, useState } from "react";
+import {ActionIcon, AppShellHeader, Burger, Flex, Title, useMantineColorScheme} from "@mantine/core";
+import {IconMoon, IconSun, IconLogout} from "@tabler/icons-react";
+import { useRouter } from 'next/router';
 
-export default function Header() {
+interface HeaderProps {
+    opened: boolean;
+    toggle: () => void;
+}
+
+export default function Header({ opened, toggle }: HeaderProps) {
+    const router = useRouter();
     const { colorScheme, setColorScheme } = useMantineColorScheme();
     const [mounted, setMounted] = useState(false);
     const dark = colorScheme === 'dark';
@@ -11,23 +18,46 @@ export default function Header() {
         setMounted(true);
     }, []);
 
+    const toggleColorScheme = () => {
+        setColorScheme(colorScheme === 'dark' ? 'light' : 'dark');
+    }
+
+    const handleLogout = () => {
+        localStorage.removeItem('userId');
+        router.push('/login');
+    };
+
     return (
         <AppShellHeader p="sm">
             <Flex justify="space-between" align="center">
-                <Title pl="1rem">Notare</Title>
-                {mounted && (
+                <Flex gap="md" align="center">
+                    <Burger opened={opened} onClick={toggle} size="sm" />
+                    <Title pl="1rem" >Notare</Title>
+                </Flex>
+                <div className="flex items-center justify-center gap-4">
+                    {mounted && (
+                        <ActionIcon
+                            variant="outline"
+                            color={dark ? 'yellow' : 'blue'}
+                            onClick={() => toggleColorScheme()}
+                            title="Toggle color scheme"
+                            size="xl"
+                        >
+                            {dark ? <IconSun size="1.6rem" /> : <IconMoon size="1.6rem" />}
+                        </ActionIcon>
+                    )}
                     <ActionIcon
                         variant="outline"
-                        color={dark ? 'yellow' : 'blue'}
-                        onClick={() => setColorScheme(dark ? 'light' : 'dark')}
+                        color={'red'}
+                        onClick={() => handleLogout()}
                         title="Toggle color scheme"
                         size="xl"
                     >
-                        {dark ? <IconSun size="1.6rem" /> : <IconMoon size="1.6rem" />}
+                        <IconLogout />
                     </ActionIcon>
-                )}
+                </div>
             </Flex>
         </AppShellHeader>
-    );
+    )
 }
 
