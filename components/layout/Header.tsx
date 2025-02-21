3bb26f95f3098c81eb0b29bfe0360b554505b5d1
MM <167732454+pqcnerd@users.noreply.github.com>
2025-02-21 16:46:21 -0500

    #15 issue resolved (#17)
    * issue #15 fixed
    
    ---------
    
    Co-authored-by: mumark <mark.mu@mail.utoronto.ca>

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,27 +1,33 @@
 import {ActionIcon, AppShellHeader, Flex, Title, useMantineColorScheme} from "@mantine/core";
 import {IconMoon, IconSun} from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 
 export default function Header() {
     const { colorScheme, setColorScheme } = useMantineColorScheme();
+    const [mounted, setMounted] = useState(false);
     const dark = colorScheme === 'dark';
-    const toggleColorScheme = () => {
-        setColorScheme(colorScheme === 'dark' ? 'light' : 'dark');
-    }
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
 
     return (
         <AppShellHeader p="sm">
             <Flex justify="space-between" align="center">
-                <Title pl="1rem" >Notare</Title>
-                <ActionIcon
-                    variant="outline"
-                    color={dark ? 'yellow' : 'blue'}
-                    onClick={() => toggleColorScheme()}
-                    title="Toggle color scheme"
-                    size="xl"
-                >
-                    {dark ? <IconSun size="1.6rem"></IconSun> : <IconMoon size="1.6rem"></IconMoon>}
-                </ActionIcon>
+                <Title pl="1rem">Notare</Title>
+                {mounted && (
+                    <ActionIcon
+                        variant="outline"
+                        color={dark ? 'yellow' : 'blue'}
+                        onClick={() => setColorScheme(dark ? 'light' : 'dark')}
+                        title="Toggle color scheme"
+                        size="xl"
+                    >
+                        {dark ? <IconSun size="1.6rem" /> : <IconMoon size="1.6rem" />}
+                    </ActionIcon>
+                )}
             </Flex>
         </AppShellHeader>
-    )
+    );
 }
+
